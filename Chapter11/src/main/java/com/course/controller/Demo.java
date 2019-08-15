3dd9b34bbbe4dc5f71fddd5947fff71d6bdec188
John Doe <johdoe@example.com>
2019-08-15 19:48:21 +0800

    Mybatis+SpringBoot关联数据进行增、删、改、查功能，代码实现

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,4 +1,48 @@
 package com.course.controller;
 
-public class DEMO {
+
+import com.course.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Log4j
+@RestController
+@Api(value = "v1",description = "这是我第一个版本的demo")
+@RequestMapping("v1")
+public class Demo {
+
+    //首先获取第一个执行sql语句对象
+    @Autowired
+    private SqlSessionTemplate template;
+
+    @RequestMapping(value = "/getUserCount",method = RequestMethod.GET)
+    @ApiOperation(value = "可以获取到所有用户数")
+    public int getUserCount(){
+        return template.selectOne("getUserCount");
+    }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public int addUser(@RequestBody User user){
+
+        return template.insert("addUser",user);
+    }
+
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
+    public int updateUser(@RequestBody User user){
+
+        return template.update("updateUser",user);
+    }
+
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.GET)
+    public int deleteUser(@RequestParam int id){
+
+        return template.delete("deleteUser",id);
+    }
+
+
+
 }
