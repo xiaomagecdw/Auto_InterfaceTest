31798e3f3ae3816c0d6e92ac209ef016cb0b8219
John Doe <johdoe@example.com>
2019-08-16 23:24:07 +0800

    增加生成报告脚本、文件读取、数据库配置、用例模板、枚举类请求地址

---
+++ Chapter12/src/main/java/com/course/model/GetUserList.java
@@ -0,0 +1,13 @@
+package com.course.model;
+
+
+import lombok.Data;
+
+@Data
+public class GetUserList {
+
+    private String userName;
+    private String age;
+    private String sex;
+    private String expected;
+}
