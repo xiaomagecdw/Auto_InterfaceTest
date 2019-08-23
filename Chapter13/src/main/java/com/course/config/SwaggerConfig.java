e70f45b698c7d308d0d1a64fca78771b0b346a2a
John Doe <johdoe@example.com>
2019-08-23 22:36:35 +0800

    开发用户模板User脚本、SwaggerConfig配置脚本、Application主程序脚本、UserManager脚本

--- Chapter13/src/main/java/com/course/config/SwaggerConfig.java
+++ Chapter13/src/main/java/com/course/config/SwaggerConfig.java
@@ -1,4 +1,37 @@
 package com.course.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .paths(PathSelectors.regex("/*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder().title("UserManager service APi")
+                .contact(new Contact("xiaomagecdw","","918971505@qq.com"))
+                .description("this is UserManager service PAI")
+                .version("1.0.0")
+                .build();
+    }
 }
